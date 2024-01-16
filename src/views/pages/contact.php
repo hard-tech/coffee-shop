b25c353b57350e6e18913e58ab29b870082b6152
Ahmad Jaber <ahamd.jaber010@yahoo.com>
2024-01-16 16:53:29 +0100

    split html in php modules

--- src/views/pages/contact.php
+++ src/views/pages/contact.php
@@ -3,81 +3,6 @@
 $title = 'contact';
 
 ob_start();?>
-<!DOCTYPE html>
-<html lang="fr">
-   <head>
-      <!-- basic -->
-      <meta charset="utf-8">
-      <meta http-equiv="X-UA-Compatible" content="IE=edge">
-      <meta name="viewport" content="width=device-width, initial-scale=1">
-      <!-- mobile metas -->
-      <meta name="viewport" content="width=device-width, initial-scale=1">
-      <meta name="viewport" content="initial-scale=1, maximum-scale=1">
-      <!-- site metas -->
-      <title>Contact</title>
-      <meta name="keywords" content="">
-      <meta name="description" content="">
-      <meta name="author" content="">
-      <!-- bootstrap css -->
-      <link rel="stylesheet" type="text/css" href="css/bootstrap.min.css">
-      <!-- style css -->
-      <link rel="stylesheet" type="text/css" href="css/style.css">
-      <!-- Responsive-->
-      <link rel="stylesheet" href="css/responsive.css">
-      <!-- fevicon -->
-      <link rel="icon" href="images/fevicon.png" type="image/gif" />
-      <!-- Scrollbar Custom CSS -->
-      <link rel="stylesheet" href="css/jquery.mCustomScrollbar.min.css">
-      <!-- Tweaks for older IEs-->
-      <link rel="stylesheet" href="https://netdna.bootstrapcdn.com/font-awesome/4.0.3/css/font-awesome.css">
-      <!-- owl stylesheets --> 
-      <link rel="stylesheet" href="css/owl.carousel.min.css">
-      <link rel="stylesheet" href="css/owl.theme.default.min.css">
-      <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/fancybox/2.1.5/jquery.fancybox.min.css" media="screen">
-   </head>
-   <body>
-      <!--header section start -->
-      <div class="header_section">
-         <div class="container-fluid">
-            <nav class="navbar navbar-expand-lg navbar-light bg-light">
-               <div class="logo"><a href="?p=home"><h1>Coffee Shop.</h1></a></div>
-               <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNav" aria-controls="navbarNav"aria-expanded="false" aria-label="Toggle navigation">
-               <span class="navbar-toggler-icon"></span>
-               </button>
-               <div class="collapse navbar-collapse" id="navbarNav">
-                  <ul class="navbar-nav ml-auto">
-                     <li class="nav-item active">
-                        <a class="nav-link" href="?p=home">Home</a>
-                     </li>
-                     <li class="nav-item">
-                        <a class="nav-link" href="?p=about">About Us</a>
-                     </li>
-                     <li class="nav-item">
-                        <a class="nav-link" href="?p=gallery">Gallery</a>
-                     </li>
-                     <li class="nav-item">
-                        <a class="nav-link" href="?p=services">Services</a>
-                     </li>
-                     <li class="nav-item">
-                        <a class="nav-link" href="?p=contact">Contact Us</a>
-                     </li>
-                     <li class="nav-item">
-                        <div class="searchIcon">
-                           <input checked="" class="checkbox" type="checkbox"> 
-                           <div class="mainbox">
-                               <div class="iconContainer">
-                                   <svg viewBox="0 0 512 512" height="1em" xmlns="http://www.w3.org/2000/svg" class="search_icon"><path d="M416 208c0 45.9-14.9 88.3-40 122.7L502.6 457.4c12.5 12.5 12.5 32.8 0 45.3s-32.8 12.5-45.3 0L330.7 376c-34.4 25.2-76.8 40-122.7 40C93.1 416 0 322.9 0 208S93.1 0 208 0S416 93.1 416 208zM208 352a144 144 0 1 0 0-288 144 144 0 1 0 0 288z"></path></svg>
-                               </div>
-                            <input class="search_input" placeholder="Rechecher" type="text">
-                           </div>
-                       </div>
-                     </li>
-                  </ul>
-               </div>
-            </nav>
-         </div>
-      </div>
-      <!--header section end -->
       <!-- contact section start -->
       <div class="contact_section layout_padding">
          <div class="container">
@@ -165,24 +90,5 @@ ob_start();?>
             </div>
          </div>
       </div>
-      <!-- footer section end -->
-      <!-- copyright section start -->
-      <div class="copyright_section">
-         <div class="container">
-            <p class="copyright_text">2020 All Rights Reserved. Design by <a href="https://html.design">Free html  Templates</a></p>
-         </div>
-      </div>
-      <!-- copyright section end -->
-      <!-- Javascript files-->
-      <script src="js/jquery.min.js"></script>
-      <script src="js/popper.min.js"></script>
-      <script src="js/bootstrap.bundle.min.js"></script>
-      <script src="js/jquery-3.0.0.min.js"></script>
-      <script src="js/plugin.js"></script>
-      <!-- sidebar -->
-      <script src="js/jquery.mCustomScrollbar.concat.min.js"></script>
-      <script src="js/custom.js"></script>     
-   </body>
-</html>
-<?php
+         <?php
 $page_content = ob_get_clean();
--- src/views/partials/navbar.php
+++
@@ -1,8 +0,0 @@
-<?php
-
-ob_start();?>
-<ul>
-    <li><a href="/?p=register">Go to Register</a></li>
-</ul>
-<?php
-$navbar = ob_get_clean();
