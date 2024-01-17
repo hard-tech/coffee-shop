0b5bf3b84bc582e0301feb73b4985fb342b6ac7d
Eliott <eliottcor2005@gmail.com>
2024-01-17 11:42:30 +0100

    insert connect db info

--- src/db.php
+++ src/db.php
@@ -1,8 +1,8 @@
 <?php
 
 try {
-    $dsn = 'mysql:host=localhost;dbname=library;port=8889';
-    $db = new PDO($dsn, 'root', 'root');
+    $dsn = 'mysql:host=coffee-time-api.new-webtech.fr;dbname=coffee_shop;port=3306';
+    $db = new PDO($dsn, 'ddev', 'ddev');
     $db->setAttribute(PDO::ATTR_DEFAULT_FETCH_MODE, PDO::FETCH_ASSOC);
 }
 catch (Exception $e) {
--- src/views/pages/admin.php
+++ src/views/pages/admin.php
@@ -4,39 +4,43 @@ $title = 'admin';
 
 ob_start();
 
-if ($_SERVER['REQUEST_METHOD'] === 'POST') {
-    // Handle add, update, or delete product form submission
 
-    if (isset($_POST['delete'])) {
-        // Handle delete product form submission
-        $id = $_POST['id'];
+// if ($_SERVER['REQUEST_METHOD'] === 'POST') {
+//     // Handle add, update, or delete product form submission
 
-        // Process the data or call your update function
-        // updateDataOnDb("", "DELETE", $apiUrl . $id);
-    } else {
-        // Handle add or update product form submission
-        $id = isset($_POST['id']) ? $_POST['id'] : null;
-        $name = $_POST['name'];
-        $pictureProd = $_POST['pictureProd'];
-        $price = $_POST['price'];
-        $inStock = isset($_POST['inStock']) ? true : false;
+//     if (isset($_POST['delete'])) {
+//         // Handle delete product form submission
+//         $id = $_POST['id'];
 
-        if ($_SERVER['REQUEST_METHOD'] === 'POST') {
-            // Process the data or call your update function
-            // updateDataOnDb(json_encode(...), "POST", $apiUrl);
-        } elseif ($_SERVER['REQUEST_METHOD'] === 'PUT') {
-            // Process the data or call your update function
-            // updateDataOnDb(json_encode(...), "PUT", $apiUrl . $id);
-        }
-    }
-}
+//         // Process the data or call your update function
+//         // updateDataOnDb("", "DELETE", $apiUrl . $id);
+//     } else {
+//         // Handle add or update product form submission
+//         $id = isset($_POST['id']) ? $_POST['id'] : null;
+//         $name = $_POST['name'];
+//         $pictureProd = $_POST['pictureProd'];
+//         $price = $_POST['price'];
+//         $inStock = isset($_POST['inStock']) ? true : false;
+
+//         if ($_SERVER['REQUEST_METHOD'] === 'POST') {
+//             // Process the data or call your update function
+//             // updateDataOnDb(json_encode(...), "POST", $apiUrl);
+//         } elseif ($_SERVER['REQUEST_METHOD'] === 'PUT') {
+//             // Process the data or call your update function
+//             // updateDataOnDb(json_encode(...), "PUT", $apiUrl . $id);
+//         }
+//     }
+// } 
+
+
+
 
 $props = [
     [1, "Caffé Capsule Carte l'or", 23, "https://media.carrefour.fr/medias/e12fed5bfd1039e2adffc3acdce74704/p_540x540/08711000360835-c1n1-s05.jpg"],
     [1, "Caffé Capsule Carte l'or", 23, "https://media.carrefour.fr/medias/e12fed5bfd1039e2adffc3acdce74704/p_540x540/08711000360835-c1n1-s05.jpg"]
 ];
 
-?>
+?> 
 
 <div id='manage-prod'>
     <div id="product">
