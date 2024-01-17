16cf65b2cd280a997bda022f5a3438a23ff80b9d
Youtmax <youtmax654@gmail.com>
2024-01-17 08:51:09 +0100

    register page is added

--- src/views/pages/register.php
+++ src/views/pages/register.php
@@ -1,20 +1,42 @@
 <?php
 
-$title = 'register';
 
-ob_start();?>
-<div>
-    <h1>PAGE REGISTER</h1>
-    
-    <?php require_once __DIR__ . '/../partials/show_error.php'; ?>
-    
-    <form action="/actions/register.php" method="POST">
-        <input type="email" name="email">
-        <input type="text" name="username">
-        <input type="password" name="password">
-        <input type="password" name="cpassword">
-        <button type="submit">Register now!</button>
-    </form>
-</div>
-<?php
-$page_content = ob_get_clean();
+
+?>
+
+<!DOCTYPE html>
+<html lang="fr">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="../../../www/assets/css/register.css">
+    <title>Inscription</title>
+</head>
+
+<body>
+    <div class="login-form-container">
+        <form class="login-form" action="../../../www/actions/register.php" method="POST">
+            <h2>Inscription</h2>
+            <div class="input-group">
+                <label for="username">Nom complet</label>
+                <input type="text" id="username" name="username" placeholder="Entrez votre nom d'utilisateur">
+            </div>
+            <div class="input-group">
+                <label for="email">E-mail</label>
+                <input type="email" id="email" name="email" placeholder="Entrez votre adresse e-mail">
+            </div>
+            <div class="input-group">
+                <label for="password">Mot de passe</label>
+                <input type="password" id="password" name="password" placeholder="Entrez votre mot de passe">
+            </div>
+            <div class="input-group">
+                <label for="password">Confirmation mot de passe</label>
+                <input type="password" id="cpassword" name="cpassword" placeholder="Confirmer votre mot de passe">
+            </div>
+            <button type="submit">S'inscrire</button>
+        </form>
+    </div>
+</body>
+
+</html>
