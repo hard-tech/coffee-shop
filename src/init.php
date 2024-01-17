5ea6a42fafc9c03dff69e57d1ec2986825505707
Eliott <eliottcor2005@gmail.com>
2024-01-17 10:10:17 +0100

    add admin page

--- src/init.php
+++ src/init.php
@@ -4,7 +4,7 @@ session_start();
 // require_once __DIR__ . '/db.php';
 
 // config du site
-$existing_pages = ['home', 'login', 'register', 'services', 'gallery', 'contact', 'about'];
+$existing_pages = ['home', 'login', 'register', 'services', 'gallery', 'contact', 'about', 'admin'];
 
 // les classes
 require_once __DIR__ . '/class/User.php';
---
+++ src/views/pages/admin.php
@@ -0,0 +1,10 @@
+<?php
+
+$title = 'admin';
+
+ob_start();?>
+<div>
+    Admin
+</div>
+<?php
+$page_content = ob_get_clean();
